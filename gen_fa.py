8f63fd7bd6cb6375ac3e1bbf5531e85fb29eb6fc
topshenyi-web <erxu@ctemc.org>
2024-04-08 18:23:32 -0400

    added like one line of code

--- gen_fa.py
+++ gen_fa.py
@@ -1,9 +1,12 @@
 # python gen_fa.py ; ./bin/gmap cdna2.fa -g genetest2.fa -E genomic -f 3 -A
 
+import os
 
 from Bio import SeqIO,SeqRecord,Seq
 
-new_genome = SeqRecord.SeqRecord(Seq.Seq("AAAAAAAAAA"*100),id="SOMEID",name="ultimate gene",description="hey");
-cdna = SeqRecord.SeqRecord(Seq.Seq("GGGGGG"+"AAAAAAAAAAAA"*5+"GGGG"),id="cdna",name="kk",description="hey");
+genome = "GATGATTTTCCCGTGTGTCCCATGTAAAAAA"*1000
+new_genome = SeqRecord.SeqRecord(Seq.Seq(genome),id="SOMEID",name="ultimate gene",description="hey")
+cdna = SeqRecord.SeqRecord(Seq.Seq(genome[200:250:1]),id="cdna",name="kk",description="hey")
 SeqIO.write(new_genome, "genetest2.fa", "fasta")
 SeqIO.write(cdna, "cdna2.fa", "fasta")
+os.system("./bin/gmap cdna2.fa -g genetest2.fa -E genomic -f 3 -A")
